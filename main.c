c3d5d8dc78ec5446eb0a4190f7bbf9a70484c8e3
Devika-tech <manodevioutlook.453@gmail.com>
2024-11-06 20:38:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    char alphabet;
+    scanf("%c",&alphabet);
+    printf("Single alphabet: %c\n", alphabet);
+
+
+    char word[];
+    scanf("%c",&word);
+    printf("Word: %s\n", word);
+
+
+    char sentence[];
+    scanf("%c",&sentence);
+    printf("Sentence: %s\n", sentence);
+}
