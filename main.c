a8e893e4437b4495264537fb1dbc3c095bb0fb86
Devika-tech <manodevioutlook.453@gmail.com>
2024-11-06 20:54:42 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    int age,monthly income;
+    scanf("%d",&age);
+    scanf("%d",&monthly income);
+     if (18 <= age <= 25)
+        if monthly income >= 5000:
+            printf("Eligible");
+    elif (26 <= age <= 40)
+        if monthly income >= 10000:
+            printf("Eligible");
+    elif (41 <= age <= 60)
+        if monthly_income >= 15000:
+           printf("Eligible");
+    else
+        printf("Not Eligible");
+
+}
