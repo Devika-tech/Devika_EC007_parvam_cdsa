cd9d05819a2f79d46b405654c73970bf3ceb7b8a
Devika-tech <manodevioutlook.453@gmail.com>
2024-11-06 20:42:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    switch(n)
+    {
+
+        case1:
+            printf("ONE");
+            break;
+        case2:
+            printf("TWO");
+            break;
+        case3:
+            printf("THREE");
+            break;
+        case4:
+            printf("FOUR");
+            break;
+        case5:
+            printf("FIVE");
+            break;
+        case6:
+            printf("SIX");
+            break;
+        case7:
+            printf("SEVEN");
+            break;
+        case8:
+            printf("EIGHT");
+            break;
+        case9:
+            printf("NINE");
+            break;
+
+    }
+}
