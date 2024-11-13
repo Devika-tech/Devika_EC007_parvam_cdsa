4b326aa570e79760d96b6a21bf0a5e0e2f5f086c
Devika-tech <manodevioutlook.453@gmail.com>
2024-11-13 11:45:01 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void reverseArray(int *arr, int size) {
+    int *start = arr;           // Pointer to the beginning of the array
+    int *end = arr + size - 1;   // Pointer to the end of the array
+
+    while (start < end) {
+        // Swap the elements pointed by start and end
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        // Move the pointers towards each other
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+
+    // Input the size of the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input the elements of the array
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Call the function to reverse the array
+    reverseArray(arr, n);
+
+    // Display the reversed array
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
+
